3c1fb655aa7dca5f3e1202fdfcc75438e0b611ac
andre <andrezhang4246@gmail.com>
2024-03-07 09:48:30 -0600

    added new shader class to aid in writing shaders

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -12,19 +13,6 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 
-//shader source code
-    const char *vertexShaderSource =    "#version 330 core\n"
-                                        "layout (location = 0) in vec3 aPos;\n"
-                                        "void main(){\n"
-                                        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                        "}\0";
-
-    const char *fragmentShaderSource =  "#version 330 core\n"
-                                        "out vec4 FragColor;\n"
-                                        "uniform vec4 ourColor;\n"
-                                        "void main(){\n"
-                                        "   FragColor = ourColor;\n"
-                                        "}\0";
 
 int main()
 {
@@ -37,6 +25,7 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
+    //-----------INTIIALIZE GLFW AND LOAD OPENGL FUNCTION POINTERS---------------------------------
 
     // glfw window creation
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
@@ -56,54 +45,12 @@ int main()
         glfwTerminate();
         return -1;
     }    
-    
-    //compile and link vertex and fragment shader
-    int success;
-    char infoLog[512];
+    //-----------FINISHED INITIALIZING GLFW AND LOADING OPENGL FUNCTION POINTERS-------------------
 
-    //compile vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        printf("ERROR: FAILED TO COMPILE VERTEX SHADER\n%s", infoLog);
-        glfwTerminate();
-        return -1;
-    }
 
-    //compile fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if(!success){
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        printf("ERROR: FAILED TO COMPILE FRAGMENT SHADER\n%s", infoLog);
-        glfwTerminate();
-        return -1;
-    }
-
-    //link vertex/fragment shader w/ the shader program
-
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success){
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        printf("ERROR: SHADER PROGRAM LINKING FAILED");
-        glfwTerminate();
-        return -1;
-    }
-
-    //delete the unused shaders
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // all intialization of window/context and shaders completed
+    //------------------------------------INTIALIZE SHADERS----------------------------------------
+    Shader myShader("VertexShader.glsl", "FragmentShader.glsl");
+    //------------------------------------DONE INTIALIZING SHADERS---------------------------------
 
     float firstTriangle[] = {
         //positions           //colors
@@ -113,13 +60,13 @@ int main()
     };
 
 
-    unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(2, VAOs); //generate multiple VAOs or buffers at the same time
-    glGenBuffers(2, VBOs);
-    // first triangle setup
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // set up info for 1st triangle
     // --------------------
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
 
     //position attribute
@@ -127,7 +74,7 @@ int main()
     glEnableVertexAttribArray(0);
 
     //color attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
 
@@ -149,20 +96,16 @@ int main()
         // -----
         processInput(window);
 
-        // render
-        // ------
+        //fill background
+        // -----------------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //activate shader program before calling glUniform
-        glUseProgram(shaderProgram);
-
-        //update shader uniform based on running time
-        float greenVal = (float)(sin(glfwGetTime())/2.0 + .25);
-        int vertexColorLoc = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLoc, 0.0f, greenVal, 0.0, 1.0f);
 
         //render triangle
+        //---------------
+        myShader.use();
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         
@@ -173,11 +116,9 @@ int main()
         glfwPollEvents();
     }
 
-    //deallocate all glfw resources
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
-    glDeleteProgram(shaderProgram);
-
+    //deallocate resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
