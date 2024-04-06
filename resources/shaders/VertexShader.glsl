435be21011e41bce8dc74cd6fc326fd495d34a30
andre <andrezhang4246@gmail.com>
2024-04-05 19:43:51 -0500

    removed redundant comments for Vertex Shader

--- resources/shaders/VertexShader.glsl
+++ resources/shaders/VertexShader.glsl
@@ -1,20 +1,3 @@
-// #version 330 core
-// layout (location = 0) in vec3 aPos;     // the position variable has attribute position 0
-// layout (location = 1) in vec3 aColor;   //color variable has attribute position 1
-// layout (location = 2) in vec2 aText;    //texture ocordinates
-
-// out vec3 ourColor;  // specify a color output to the fragment shader
-// out vec2 texCoord; // specify texture coordinates for a vertex
-
-// uniform float scale;
-
-// void main()
-// {
-//     gl_Position = vec4(aPos.x * scale, aPos.y * scale, aPos.z * scale, 1.0);
-//     ourColor = aColor;
-//     texCoord = aText;
-// }
-
 #version 330 core
 
 // Positions/Coordinates
